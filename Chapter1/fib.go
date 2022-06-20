c83ab03777d313b6c5e20cb355fda00926190bf2
William Doran <wfdoran4@yahoo.com>
2022-06-20 06:50:09 -0700

    Added fib5() and fib6() to fib.go.

--- Chapter1/fib.go
+++ Chapter1/fib.go
@@ -20,12 +20,48 @@ func fib3(n int) int {
 	return memo[n]
 }
 
+func fib5(n int) int {
+	if n == 0 {
+		return 0
+	}
+	last := 0
+	next := 1
+	for i := 1; i < n; i++ {
+		last, next = next, last+next
+	}
+	return next
+}
+
+func fib6(n int) <-chan int {
+	ch := make(chan int)
+
+	go func(ch chan int) {
+		ch <- 0
+
+		if n > 0 {
+			last := 0
+			next := 1
+			for i := 1; i < n; i++ {
+				last, next = next, last+next
+				ch <- last
+			}
+		}
+		close(ch)
+	}(ch)
+
+	return ch
+}
+
 func main() {
 	memo = make(map[int]int)
 	memo[0] = 0
 	memo[1] = 1
 
 	for i := 0; i < 20; i++ {
-		fmt.Println(i, fib2(i), fib3(i))
+		fmt.Println(i, fib2(i), fib3(i), fib5(i))
+	}
+
+	for x := range fib6(20) {
+		fmt.Println(x)
 	}
 }
