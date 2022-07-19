ad4f699e46898e47b0ea18364629978d0785b458
William Doran <wfdoran4@yahoo.com>
2022-07-18 19:59:45 -0700

    Refactored CSP structs.  maps/sets instead of arrays for variables and domains.

--- Chapter3/csp.go
+++ Chapter3/csp.go
@@ -1,9 +1,41 @@
 package main
 
-type constraint_checker[V comparable, D any] func(map[V]D) bool
+type constraint_checker[V comparable, D comparable] func(map[V]D) bool
 
-type CSP[V any, D any] struct {
-	variables   []V
-	domains     []D
-	constraints []constraint_checker
+type Constraint[V comparable, D comparable] struct {
+	inputs []V
+	check  constraint_checker[V, D]
+}
+
+type CSP[V comparable, D comparable] struct {
+	variables   map[V]bool
+	domains     map[D]bool
+	constraints []Constraint[V, D]
+}
+
+func New[V comparable, D comparable]() *CSP[V, D] {
+	return &CSP[V, D]{variables: make(map[V]bool), domains: make(map[D]bool), constraints: nil}
+}
+
+func New2[V comparable, D comparable](variables []V, domains []D) *CSP[V, D] {
+	csp := New[V, D]()
+	for _, v := range variables {
+		csp.variables[v] = true
+	}
+	for _, d := range domains {
+		csp.domains[d] = true
+	}
+	return csp
+}
+
+func (csp *CSP[V, D]) AddVariable(v V) {
+	csp.variables[v] = true
+}
+
+func (csp *CSP[V, D]) AddDomain(d D) {
+	csp.domains[d] = true
+}
+
+func (csp *CSP[V, D]) AddConstraint(c Constraint[V, D]) {
+	csp.constraints = append(csp.constraints, c)
 }
