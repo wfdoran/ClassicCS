c0a0265251ac8bb8ee7c4cffa913cf71b4db3fa8
William Doran <wfdoran4@yahoo.com>
2022-08-09 17:36:08 -0700

    Section 5.5 Compressed size 90 or 91, skipping this one.

--- Chapter5/compress.go
+++ Chapter5/compress.go
@@ -4,6 +4,8 @@ import (
 	"bytes"
 	"compress/gzip"
 	"fmt"
+	"math/rand"
+	"time"
 )
 
 type data struct {
@@ -13,25 +15,31 @@ type data struct {
 func CFitness(d data) float64 {
 	var b bytes.Buffer
 	gz := gzip.NewWriter(&b)
+
 	for _, name := range d.names {
 		gz.Write([]byte(name))
 	}
 	gz.Flush()
 	gz.Close()
 
-	fmt.Println(b.Bytes())
+	// fmt.Println(b.Bytes())
 	return float64(len(b.Bytes()))
 }
 
 func main() {
+	rand.Seed(time.Now().UnixNano())
+
 	people := []string{"Michael", "Sarah", "Joshua", "Narine", "David",
 		"Sajid", "Melanie", "Daniel", "Wei", "Dean", "Brian", "Murat", "Lisa"}
 
+	m := len(people)
+	for i := 1; i < m; i++ {
+		j := rand.Intn(i + 1)
+		people[i], people[j] = people[j], people[i]
+	}
+
 	n := data{names: people}
 
-	fmt.Println(CFitness(n))
-
-	n.names[0], n.names[3] = n.names[3], n.names[0]
-
+	fmt.Println(people)
 	fmt.Println(CFitness(n))
 }
