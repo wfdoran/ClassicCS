a736ac548019c3f4e0dc787c79593ad9559dcf59
William Doran <wfdoran4@yahoo.com>
2022-08-24 17:02:30 -0700

    Section 7.4.1 Neuron struct.

--- nn/nn.go
+++ nn/nn.go
@@ -18,3 +18,29 @@ func DerivativeSigmoid(x float64) float64 {
 	s := Sigmoid(x)
 	return s * (1.0 - s)
 }
+
+type ActivationFunc func(float64) float64
+type Neuron struct {
+	weights       []float64
+	learning_rate float64
+	activation    ActivationFunc
+	derivative    ActivationFunc
+	output_cache  float64
+	delta         float64
+}
+
+func NewNeuron(weights []float64, learning_rate float64) *Neuron {
+	return &Neuron{
+		weights:       weights,
+		learning_rate: learning_rate,
+		activation:    Sigmoid,
+		derivative:    DerivativeSigmoid,
+		output_cache:  0.0,
+		delta:         0.0,
+	}
+}
+
+func (n *Neuron) Output(inputs []float64) float64 {
+	n.output_cache = DotProduct(n.weights, inputs)
+	return n.output_cache
+}
